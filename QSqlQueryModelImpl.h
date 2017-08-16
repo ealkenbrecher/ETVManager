98a0f55f23110d34ed6b593b1cedaa2aef133b50
EAlkenbrecher <hv@alkenbrecher.info>
2017-08-16 16:54:35 +0200

    added missing syntax

--- QSqlQueryModelImpl.h
+++ QSqlQueryModelImpl.h
@@ -6,26 +6,27 @@
 
 class QSqlQueryModelImpl : public QSqlQueryModel
 {
-public:
-  QSqlQueryModelImpl (QObject *parent = 0);
-  QVariant data(const QModelIndex &index, int role) const;
-  void setDbConnectionName (QString &dbName);
-  void setPropertyId (int propId);
-  void setYear (int year);
-  void setAgendaNum (int agendaNum);
-  void moveRowUp (int aSourceRow);
-  void moveRowDown (int aSourceRow);
+  public:
+    QSqlQueryModelImpl (QObject *parent = 0);
+    virtual ~QSqlQueryModelImpl();
+    QVariant data(const QModelIndex &index, int role) const;
+    void setDbConnectionName (QString &dbName);
+    void setPropertyId (int propId);
+    void setYear (int year);
+    void setAgendaNum (int agendaNum);
+    //void moveRowUp (int aSourceRow);
+    //void moveRowDown (int aSourceRow);
 
-  QString getDbConnectionName () {return mDbConnectionName;};
-  int getPropertyId () {return mPropertyId;}
-  int getYear () {return mYear;}
-  int getAgendaNum () {return mAgendaNum;}
+    QString getDbConnectionName () {return mDbConnectionName;};
+    int getPropertyId () {return mPropertyId;};
+    int getYear () {return mYear;};
+    int getAgendaNum () {return mAgendaNum;};
 
-private:
-  QString mDbConnectionName;
-  int mPropertyId;
-  int mYear;
-  int mAgendaNum;
+  private:
+    QString mDbConnectionName;
+    int mPropertyId;
+    int mYear;
+    int mAgendaNum;
 };
 
 #endif // QSqlQueryModelImpl_H
